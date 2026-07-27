0da015d611d7b07189222147183f20b331bbfba8
IoriBranford <ioribranford@gmail.com>
2026-07-27 07:39:46 -0700

    Prevent overwriting dispatch functions with listener arrays

--- dispatch.lua
+++ dispatch.lua
@@ -7,12 +7,13 @@ local tnew = require "table.new"
 ---@field free integer[]?
 
 ---@class dispatch
----@field [string] listeners
+---@field events table<string, listeners>
 local dispatch = {}
 dispatch.__index = dispatch
 
 function dispatch.new(...)
-    local self = tnew(0, 16) ---@type dispatch
+    local self = tnew(0, 1) ---@type dispatch
+    self.events = tnew(0, 16)
     setmetatable(self, dispatch)
     for i = 1, select("#", ...) do
         local ev = select(i, ...)
@@ -22,8 +23,8 @@ function dispatch.new(...)
 end
 
 function dispatch:newevent(ev)
-    if not self[ev] then
-        self[ev] = tnew(8, 1)
+    if not self.events[ev] then
+        self.events[ev] = tnew(8, 1)
     end
 end
 
@@ -35,7 +36,7 @@ function dispatch:sub(ev, l, after)
     assert(type(l[ev]) == "function")
 
     self:newevent(ev)
-    local ls = self[ev]
+    local ls = self.events[ev]
 
     local free = ls.free
     local i = not after and
@@ -56,8 +57,9 @@ function dispatch:allsub(l, after, force)
             end
         end
     else
+        local evs = self.events
         for ev, f in pairs(l) do
-            if self[ev] and type(f) == "function" then
+            if evs[ev] and type(f) == "function" then
                 l[ev.."sub"] = sub(self, ev, l, after)
             end
         end
@@ -69,7 +71,7 @@ end
 ---@param i integer
 ---@param l listener?
 function dispatch:unsub(ev, i, l)
-    local ls = self[ev]
+    local ls = self.events[ev]
     if not ls then return end
 
     if l then assert(l == ls[i]) end
@@ -92,7 +94,7 @@ function dispatch:allunsub(l)
 end
 
 function dispatch:clearev(ev)
-    self[ev] = nil
+    self.events[ev] = nil
 end
 
 local function send(ls, i1, i2, di, ev, ...)
@@ -110,22 +112,22 @@ local function sendself(ls, i1, i2, di, ev, ...)
 end
 
 function dispatch:send(ev, ...)
-    local ls = self[ev]
+    local ls = self.events[ev]
     if ls then send(ls, 1, #ls, 1, ev, ...) end
 end
 
 function dispatch:rsend(ev, ...)
-    local ls = self[ev]
+    local ls = self.events[ev]
     if ls then send(ls, #ls, 1, -1, ev, ...) end
 end
 
 function dispatch:sendself(ev, ...)
-    local ls = self[ev]
+    local ls = self.events[ev]
     if ls then sendself(ls, 1, #ls, 1, ev, ...) end
 end
 
 function dispatch:rsendself(ev, ...)
-    local ls = self[ev]
+    local ls = self.events[ev]
     if ls then sendself(ls, #ls, 1, -1, ev, ...) end
 end
 
