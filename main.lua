c3f96284863a23144afb5b680b69c7b85e57e3c6
IoriBranford <ioribranford@gmail.com>
2026-07-27 10:54:31 -0700

    Main loop using dispatch that preserves normal love DX

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 local dispatch = require "dispatch"
 local FS = love.filesystem
+local GX = love.graphics
 
 FS.setRequirePath(table.concat({
     FS.getRequirePath(),
@@ -9,15 +10,25 @@ FS.setRequirePath(table.concat({
     "libraries/?/init.lua",
 }, ';'))
 
+local function drawLoadError(i, err)
+    local mrg = 10
+    local w = GX.getWidth()
+    local font = GX.getFont()
+    local fh = font:getHeight()
+    local x, y = mrg, fh * i
+    w = w - 2 * mrg
+    GX.printf(err, x, y, w, "left")
+end
+
 ---@diagnostic disable-next-line: duplicate-set-field
-function love.run()
+function love.load()
     local cli = FS.getIdentity()
     if not love.filesystem.isFused() then
         cli = cli .. [[
             <game> (string)         Game assets location
         ]]
     end
-    cli = cli..[[
+    cli = cli .. [[
         --console               Output to a console window
         --version               Print LOVE version
         --fused                 Force running in fused mode
@@ -27,7 +38,7 @@ function love.run()
         <files...> (optional string)   One or more LOVE programs to run
     ]]
 
-    local args = require "pl.lapp"(cli)
+    local args = require "pl.lapp" (cli)
 
     if args.debug then
         require("lldebugger").start()
@@ -40,51 +51,59 @@ function love.run()
         profile.start("Fli1", filename)
     end
 
-    local GX = love.graphics
+    -- local files = args.files
+    -- local function loadf(i, file)
+    --     local ft, err = LW:pushfile(file)
+    --     if ft then return end
 
-    local function drawLoadError(i, err)
-        local mrg = 10
-        local w = GX.getWidth()
-        local font = GX.getFont()
-        local fh = font:getHeight()
-        local x, y = mrg, fh*i
-        w = w - 2*mrg
-        GX.printf(err, x, y, w, "left")
-    end
+    --     err = string.format("%d. %s: %s", i, file, err)
+    --     print(err)
+    --     LW[#LW+1] = {
+    --         draw = function() drawLoadError(i, err) end
+    --     }
+    -- end
 
-    local dsph = dispatch.new(
-        "load",
-        "update",
-        "draw",
-        "quit"
-    )
+    -- local i1 = FS.isFused() and 1 or 2
+    -- for i = i1, #files do
+    --     loadf(i, files[i])
+    -- end
 
-    local files = args.files
-    local function loadf(i, file)
-        -- local ft, err = LW:pushfile(file)
-        -- if ft then return end
+    -- if #Dispatch.events.draw <= 0 then
+    --     Dispatch:allsub({
+    --         draw = function()
+    --             drawLoadError(1, "No code files")
+    --         end
+    --     })
+    -- end
+    GX.setNewFont(64)
+end
 
-        -- err = string.format("%d. %s: %s", i, file, err)
-        -- print(err)
-        -- LW[#LW+1] = {
-        --     draw = function() drawLoadError(i, err) end
-        -- }
-    end
+local a = 0
 
-    local i1 = FS.isFused() and 1 or 2
-    for i = i1, #files do
-        loadf(i, files[i])
-    end
+---@diagnostic disable-next-line: duplicate-set-field
+love.update = function (dt)
+    a = a + dt
+end
 
-    if #dsph.events.draw <= 0 then
-        dsph:allsub({
-            draw = function()
-                drawLoadError(1, "No code files")
-            end
-        })
-    end
+---@diagnostic disable-next-line: duplicate-set-field
+love.draw = function()
+    local ghw = GX.getWidth()/2
+    local ghh = GX.getHeight()/2
+    local fhh = GX.getFont():getHeight()/2
+    GX.printf("love.eventconnect", ghw, ghh, 2*ghh, "center", a, 1, 1, ghh, fhh)
+end
 
-    GX.setNewFont(16)
+local Dispatch = dispatch.new("audiodisconnected", "directorydropped", "displayrotated", "draw", "dropbegan",
+    "dropcompleted", "dropmoved", "exposed", "filedropped", "focus", "gamepadaxis", "gamepadpressed", "gamepadreleased",
+    "joystickadded", "joystickaxis", "joystickhat", "joystickpressed", "joystickreleased", "joystickremoved",
+    "joysticksensorupdated", "keypressed", "keyreleased", "load", "localechanged", "lowmemory", "mousefocus",
+    "mousemoved", "mousepressed", "mousereleased", "occluded", "quit", "resize", "sensorupdated", "textedited",
+    "textinput", "threaderror", "touchmoved", "touchpressed", "touchreleased", "update", "visible", "wheelmoved")
+
+---@diagnostic disable-next-line: duplicate-set-field
+function love.run()
+    Dispatch:allsub(love)
+    Dispatch:send("load", love.arg.parseGameArguments(arg), arg)
 
     -- We don't want the first frame's dt to include time taken by love.load.
     if love.timer then love.timer.step() end
@@ -96,13 +115,13 @@ function love.run()
         -- Process events.
         if love.event then
             love.event.pump()
-            for name, a,b,c,d,e,f in love.event.poll() do
+            for name, a, b, c, d, e, f in love.event.poll() do
                 if name == "quit" then
                     if not love.quit or not love.quit() then
                         return "quit", a or 0
                     end
                 end
-                dsph:send(name, a, b, c, d, e, f)
+                Dispatch:send(name, a, b, c, d, e, f)
             end
         end
 
@@ -110,13 +129,13 @@ function love.run()
         if love.timer then dt = love.timer.step() end
 
         -- Call update and draw
-        dsph:send("update", dt) -- will pass 0 if love.timer is disabled
+        Dispatch:send("update", dt) -- will pass 0 if love.timer is disabled
 
         if love.graphics and love.graphics.isActive() then
             love.graphics.origin()
             love.graphics.clear(love.graphics.getBackgroundColor())
 
-            dsph:send("draw")
+            Dispatch:send("draw")
 
             love.graphics.present()
         end
